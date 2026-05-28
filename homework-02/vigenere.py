f75e5272d2c68cd22cf0ca3293ac17942c35d0b3
Anastasia6780 <nasyrtia@gmail.com>
2026-05-28 11:59:51 +0300

    Update vigenere.py

--- homework-02/vigenere.py
+++ homework-02/vigenere.py
@@ -9,22 +9,40 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
+
+    lowercase_start = ord('a')
+    uppercase_start = ord('A')
+    alphabet_size = 26
     key_len = len(keyword)
-    key_index = 0
 
-    for ch in plaintext:
-        if ch.isalpha():
-            shift = ord(keyword[key_index % key_len].lower()) - ord('a')
-            if ch.isupper():
-                shifted = chr((ord(ch) - ord('A') + shift) % 26 + ord('A'))
-            else:
-                shifted = chr((ord(ch) - ord('a') + shift) % 26 + ord('a'))
-            ciphertext.append(shifted)
+    ciphertext = ""
+
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+
+        key_letter_ord = ord(keyword[i % key_len])
+        if (key_letter_ord >= lowercase_start) and (key_letter_ord < lowercase_start + alphabet_size):
+            shift = key_letter_ord - lowercase_start
+        elif (key_letter_ord >= uppercase_start) and (key_letter_ord < uppercase_start + alphabet_size):
+            shift = key_letter_ord - uppercase_start
         else:
-            ciphertext.append(ch)
-        key_index += 1
+            shift = 0
 
-    return ''.join(ciphertext)
+        if (ord(char) >= lowercase_start) and (ord(char) < lowercase_start + alphabet_size):
+            new_char = chr((ord(char) - lowercase_start + shift) % alphabet_size + lowercase_start)
+            ciphertext += new_char
+        elif (ord(char) >= uppercase_start) and (ord(char) < uppercase_start + alphabet_size):
+            new_char = chr((ord(char) - uppercase_start + shift) % alphabet_size + uppercase_start)
+            ciphertext += new_char
+        else:
+            ciphertext += char
+
+    return ciphertext
+
+
+
+    # PUT YOUR CODE HERE
+    return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -38,20 +56,31 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext = []
+    lowercase_start = ord('a')
+    uppercase_start = ord('A')
+    alphabet_size = 26
     key_len = len(keyword)
-    key_index = 0
 
-    for ch in ciphertext:
-        if ch.isalpha():
-            shift = ord(keyword[key_index % key_len].lower()) - ord('a')
-            if ch.isupper():
-                shifted = chr((ord(ch) - ord('A') - shift) % 26 + ord('A'))
-            else:
-                shifted = chr((ord(ch) - ord('a') - shift) % 26 + ord('a'))
-            plaintext.append(shifted)
+    plaintext = ""
+
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+
+        key_letter_ord = ord(keyword[i % key_len])
+        if (key_letter_ord >= lowercase_start) and (key_letter_ord < lowercase_start + alphabet_size):
+            shift = key_letter_ord - lowercase_start
+        elif (key_letter_ord >= uppercase_start) and (key_letter_ord < uppercase_start + alphabet_size):
+            shift = key_letter_ord - uppercase_start
         else:
-            plaintext.append(ch)
-        key_index += 1
+            shift = 0
 
-    return ''.join(plaintext)
+        if (ord(char) >= lowercase_start) and (ord(char) < lowercase_start + alphabet_size):
+            new_char = chr((ord(char) - lowercase_start - shift) % alphabet_size + lowercase_start)
+            plaintext += new_char
+        elif (ord(char) >= uppercase_start) and (ord(char) < uppercase_start + alphabet_size):
+            new_char = chr((ord(char) - uppercase_start - shift) % alphabet_size + uppercase_start)
+            plaintext += new_char
+        else:
+            plaintext += char
+
+    return plaintext
